886364112fc82b1727c8e5eb44c0022eab3da398
youssefsoli <youssefdevelops@gmail.com>
2019-09-20 00:56:14 -0600

    Added weight support

--- src/averageCalculator.js
+++ src/averageCalculator.js
@@ -23,14 +23,13 @@ const waitForLoad = (cb) => {
 const grabMarkBooks = markBooks => {
   try {
     $('table a[onclick]').not(':last').each(function () {
-      const className = $(this).parent().parent().children().first().text(); // Holds the first 3 letters of the class
-      let currentMultiplier = 1; // Multiplier of current class
-      if (className.substr(0, 3) === 'ELA' || className.substr(0, 3) === 'MAT') // ELA and Math are both double weighted courses
-        currentMultiplier = 2; // Change weight to double
+      const $row = $(this).closest('tr'); // Store the jQuery row as a variable
+      const className = $row.find('td:first').text(); // Grab the class name from the first column
+      let multiplier = $row.find('td:nth-child(3) > input').val(); // Grab the weight from the third column
       markBooks.push({ // Adds class to array with a multiplier and its info
         name: className,
         classInfo: $(this).attr('onclick'), // Holds class information
-        multiplier: currentMultiplier // Holds multiplier/weight of class
+        multiplier: parseInt(multiplier) // Holds multiplier/weight of class
       });
     });
   } catch (e) {
@@ -82,11 +81,9 @@ const addColumnBefore = (i, name, html) => {
  * @param {Object} courseMarks Holds key value pairs of className:classScore
  */
 const addMarksToClassRows = courseMarks => {
-  for (var courseName in courseMarks) { // Loops each courseName in the courseMarks JSON Object
-    if (courseMarks.hasOwnProperty(courseName)) { // Checks if the courseName is in courseMarks
-      addMarkToClassRow(courseMarks[courseName], courseName); // Passes the mark and courseName to addMarkToClassRow
-    }
-  }
+  courseMarks.forEach(markbook => {
+    addMarkToClassRow(markbook.grade, markbook.name);
+  });
 }
 
 /**
@@ -104,24 +101,119 @@ const addMarkToClassRow = (mark, className) => {
   });
 }
 
-/**
- * @desc Calculates the weighted and normal average, which are returned through the callback
- * @param {Array} markBooks Array that holds the class info and multipliers
- * @param {Function} cb Callback, with params (weightedAverage, average)
- */
-const calculateAverage = (markBooks, cb) => {
+const calculateAverage = () => {
+  let totalGrades = 0;
+  let totalWeightedGrades = 0;
+  let totalClasses = 0;
+  let totalWeights = 0;
+  let average, weightedAverage;
+  let markBooks = JSON.parse(sessionStorage.markBooks);
+
+  markBooks.forEach(function (markbook) {
+    if (isNaN(parseInt(markbook.multiplier)) || markbook.multiplier <= 0) // If the class is given a <= 0 or no weighting
+      return;
+
+    if (isNaN(parseFloat(markbook.grade))) // If the class has no grade
+      return;
+
+    totalGrades += markbook.grade;
+    totalWeightedGrades += markbook.multiplier * markbook.grade;
+    totalClasses++;
+    totalWeights += markbook.multiplier;
+  });
+
+  if (totalClasses <= 0 || totalWeights <= 0) // Don't accept zero and negative denominators
+    return;
+
+  average = Math.round(totalGrades / totalClasses * 100) / 100;
+  weightedAverage = Math.round(totalWeightedGrades / totalWeights * 100) / 100;
+
+  $('#avg').text(average);
+  $('#weightedAvg').text(weightedAverage);
+}
+
+const setWeights = () => {
   try {
-    let courseGrades = {};
-    let sumWeighted = 0; // Holds the sum of the weighted
-    let sum = 0; // Holds the sum of the normal
-    let done = 0; // Hold the amount of async requests completed
-    let denominator = markBooks.length; // Holds the denominator for the non-weighted calculation
-    let weightedDenominator = markBooks.length; // Holds the denominator for the weighted calculation
+    if (localStorage.weights) {
+      let weights = JSON.parse(localStorage.weights);
+      weights.forEach(weight => {
+        $("#TableSecondaryClasses tr").each(function (i, row) { // Loops through each table row
+          const $row = $(row); // Get the jQuery object of the row
+          if ($row.find("td:first").text() == weight.name) { // Check if the current row is the class we are looking for
+            $row.find("td:nth-child(3) > input").val(weight.weight); // Set the value of the weight if it exists
+            return; // Stop the loop and exit the function
+          }
+        });
+      });
+    } else {
+      const weights = [];
+
+      $("#TableSecondaryClasses tr").each(function (i, row) { // Loops through each table row
+        const $row = $(row); // Get the jQuery object of the row
+        const name = $row.find("td:first").text();
+        const weight = $row.find("td:nth-child(3) > input").val();
+
+        if (!weight)
+          return;
+
+        weights.push({
+          name: name,
+          weight: parseInt(weight)
+        });
+      });
+
+      localStorage.setItem('weights', JSON.stringify(weights));
+    }
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+const updateWeights = () => {
+  const weights = [];
+  let markBooks = JSON.parse(sessionStorage.markBooks);
+
+  $("#TableSecondaryClasses tr").each(function (i, row) { // Loops through each table row
+    const $row = $(row); // Get the jQuery object of the row
+    const name = $row.find("td:first").text();
+    const weight = $row.find("td:nth-child(3) > input").val();
+
+    if (!weight)
+      return;
+
+    weights.push({
+      name: name,
+      weight: parseInt(weight)
+    });
+  });
+
+  markBooks.forEach((markbook, i, books) => {
+    weights.forEach(function (weight) {
+      if (weight.name === markbook.name) {
+        books[i].multiplier = weight.weight;
+        return;
+      }
+    });
+  });
+
+  localStorage.setItem('weights', JSON.stringify(weights));
+  sessionStorage.setItem('markBooks', JSON.stringify(markBooks));
+
+  calculateAverage();
+}
+
+/**
+ * @desc Fetches the grades for each markbook
+ * @param {Array} markBooks Array that holds the class info and multipliers
+ */
+const fetchMarkbooks = async (markBooks, cleanedMarkbooks) => {
+  try {
+    const requests = [];
     const studentID = markBooks[0].classInfo[0]; // To save proccessing, studentID is grabbed once
     /* This absolute URL method is needed since relative paths break in firefox */
     const currentURL = new URL(window.location.href); // Parse the current location as a URL object
     const postURL = currentURL.origin + currentURL.pathname + "/../../../viewer/Achieve/TopicBas/StuMrks.aspx/GetMarkbook"; // Segment and add the parts to a single string
-    markBooks.forEach((markbook) => {
+    markBooks.forEach((markbook, i, books) => {
       const classID = markbook.classInfo[1]; // Extract the classID from the second element in the markbook's info
       const termID = markbook.classInfo[2]; // Extract the termID from the third element in the markbook's info
       const topicID = markbook.classInfo[3]; // Extract the topicID from the fourth element in the markbook's info
@@ -130,7 +222,7 @@ const calculateAverage = (markBooks, cb) => {
         ", termID: " + termID +
         ", topicID: " + topicID +
         ", fromDate: '1/1/2000', toDate: '1/1/3000', relPath: '../../../'}";
-      $.ajax({
+      requests.push($.ajax({
         type: "POST", // Post request
         url: postURL, // File that holds markbooks
         data: toPost, // Post data is toPost
@@ -142,29 +234,19 @@ const calculateAverage = (markBooks, cb) => {
           if (loc === -1) // If term mark isn't found
             return;
           else {
-            if (markbook.multiplier === 2) weightedDenominator++; // If the multiplier is 2, add to the denominator
             let tempResp = response.substr(loc); // Grab everything after and including 'Term Mark: '
             tempResp = tempResp.substr(0, tempResp.indexOf('<')); // Grab everything from 'Term Mark: ' to the next '<'
             const classScore = parseFloat(tempResp.substr(11)); // Grab everything after 'Term Mark: ' and parse as float
-            if (!classScore) { // If the class does not have a valid score, remove it from the calculation
-              weightedDenominator -= markbook.multiplier;
-              denominator--;
-            } else {
-              courseGrades[markbook.name] = classScore;
-              sumWeighted += classScore * markbook.multiplier; // Multiply class score by multiplier if weighted and add to sum
-              sum += classScore; // Add score to sum
-            }
-            done++; // Increment 'done' since Ajax request and parsing has completed
-            if (done === markBooks.length) { // If its the last to finish
-              const weightedAverage = Math.round(sumWeighted / weightedDenominator * 100) / 100; // Calculate weightedAverage
-              const average = Math.round(sum / denominator * 100) / 100; // Calculate average
-              cb(weightedAverage, average, courseGrades); // Callback the function with new values as params
-            }
+            if (isNaN(classScore))
+              return;
+            books[i].grade = classScore;
+            cleanedMarkbooks.push(books[i]);
           }
         },
         error: e => console.log(e.statusText) // Log any ajax errors
-      });
+      }));
     });
+    await Promise.all(requests); // Await for all the requests to finish
   } catch (e) {
     console.log(e)
   }
@@ -175,7 +257,7 @@ const calculateAverage = (markBooks, cb) => {
  * @param {String} item Value to display
  * @param {String} itemName Name of the value being displayed
  */
-const addItemToTable = (item, itemName) => {
+const addItemToTable = (item, itemName, id) => {
   /* Table class contains the letter that corresponds to the next color in the table rows
    * Grabs the table element's last row's block's class, where the letter is extracted
    */
@@ -183,46 +265,38 @@ const addItemToTable = (item, itemName) => {
 
   /* Appends the row to the end of the table */
   $('#TableSecondaryClasses tr:last').after("<tr><td class='mwTABLE_CELL_" + tableClass +
-    "'>" + itemName + "</td><td class='mwTABLE_CELL_" + tableClass + "'>" + item + "</td></tr>");
+    "'>" + itemName + "</td><td class='mwTABLE_CELL_" + tableClass + "' id='" + id + "'>" + item + "</td></tr>");
 }
 
 /**
  * @desc Parses the injection of averages
  */
-const injectScores = () => {
+const injectScores = async () => {
   try {
     addColumnAfter(1, 'Current Mark', '<span style="color:LightGrey;">n.a.</span'); // Add the column to hold the marks
-    addColumnAfter(2, 'Weight', '<input min="0" type="number" value="5">')
-    $('#TableSecondaryClasses table').prepend('<style type="text/css">input[type="number"]::-webkit-outer-spin-button,input[type="number"]::-webkit-inner-spin-button {-webkit-appearance: none;margin: 0;} input[type="number"] {-moz-appearance: textfield; margin: 0; border: none; display: inline; font-family: Monaco, Courier, monospace; font-size: inherit; padding: 0px; text-align: center; width: 30pt; background-color: inherit;}</style>');
-    if (!sessionStorage.average || !sessionStorage.weightedAverage || !sessionStorage.courseGrades) { // If one does not exist, refetch the values
+    addColumnAfter(2, 'Weight', '<input min="0" onchange="updateWeights()" type="number" value="5">') // Add the column to hold the modifiable weights
+    setWeights(); // Sets the weightings based on localStorage
+
+    /* Inject css for the inputs */
+    $('#TableSecondaryClasses table').prepend('<style type="text/css">input[type="number"]::-webkit-outer-spin-button,input[type="number"]::-webkit-inner-spin-button {-webkit-appearance: none;margin: 0;}' +
+      'input[type="number"] {-moz-appearance: textfield; margin: 0; border: none; display: inline; font-family: Monaco, Courier, monospace; font-size: inherit; padding: 0px; text-align: center; width: 30pt; background-color: inherit;}</style>');
+
+    if (!sessionStorage.markBooks) { // If the courseGrades don't exist
       let markBooks = [];
+      let cleanedMarkbooks = [];
       grabMarkBooks(markBooks); // Grab the markbooks
       cleanseValues(markBooks); // Parse the markbooks
-      calculateAverage(markBooks, (weightedAverage, average, courseGrades) => { // Calculate averages of markbooks
-        /* Add the averages to sessionStorage which is active as long as window is open */
-        sessionStorage.setItem('weightedAverage', weightedAverage);
-        sessionStorage.setItem('average', average);
-        sessionStorage.setItem('courseGrades', JSON.stringify(courseGrades));
-
-        /* Add the averages to the table */
-        if (window.settings.calculation) {
-          addItemToTable(sessionStorage.weightedAverage, 'Average (Weighted)');
-          addItemToTable(sessionStorage.average, 'Average');
-          setTimeout(pollScores, 2000); // First call to pollScores since 'calculateAverage' is asynchronous
-        }
-        if (window.settings.quickview)
-          addMarksToClassRows(JSON.parse(sessionStorage.courseGrades));
-      });
-    } else {
-      /* Add the averages to the table */
-      if (window.settings.calculation) {
-        addItemToTable(sessionStorage.weightedAverage, 'Average (Weighted)');
-        addItemToTable(sessionStorage.average, 'Average');
-        setTimeout(pollScores, 2000); // Second call to pollScores since 'calculateAverage' is asynchronous
-      }
-      if (window.settings.quickview)
-        addMarksToClassRows(JSON.parse(sessionStorage.courseGrades));
+      await fetchMarkbooks(markBooks, cleanedMarkbooks); // Await the values of the markbooks
+      sessionStorage.setItem('markBooks', JSON.stringify(cleanedMarkbooks));
     }
+    if (window.settings.calculation) {
+      addItemToTable('<span style="color:LightGrey;">n.a.</span>', 'Average (Weighted)', 'weightedAvg');
+      addItemToTable('<span style="color:LightGrey;">n.a.</span>', 'Average', 'avg');
+      calculateAverage();
+      setTimeout(pollScores, 2000); // Second call to pollScores since 'calculateAverage' is asynchronous
+    }
+    if (window.settings.quickview)
+      addMarksToClassRows(JSON.parse(sessionStorage.markBooks));
   } catch (e) {
     console.log(e)
   }
