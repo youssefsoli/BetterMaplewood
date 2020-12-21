4bf4c233d3573b839267b778a9bc7e76a0d882a4
Michael Xu <61663933+themicklepickle@users.noreply.github.com>
2020-12-21 12:48:54 -0700

    Moved percentage column to the rightmost side
    Youssef thinks it will confuse users otherwise :(
    
    Co-authored-by: Youssef Soliman <youssefdevelops@gmail.com>

--- src/percentages.js
+++ src/percentages.js
@@ -3,21 +3,21 @@
  */
 const addPercentageColumn = () => {
     // add percent header
-    $('<td class="mwTABLE_CELL_HEADER tdAchievement" style="font-weight: bold" align="center">Percent</td>').insertAfter('#markbookTable td:first');
+    $('<td class="mwTABLE_CELL_HEADER tdAchievement" style="font-weight: bold" align="center">Percent</td>').insertAfter('#markbookTable tr:first td:last');
     
     // narrow assignment name column width
     $('#markbookTable tr:not(:first)').each(function () {
         $(this).find('td:first').css('width', '250px');
     });
 
-    // copy date column (to keep row style) and paste it after the first column
+    // copy end column (to keep row style) and paste it after the last column
     $('#markbookTable tr:not(:first)').each(function () {
-        const dateCell = $(this).find('td:nth-last-child(3)');
-        $(dateCell).clone().insertAfter($(this).find('td:first'));
+        const cell = $(this).find('td:nth-last-child(1)');
+        $(cell).clone().insertAfter($(this).find('td:last'));
     });
 
     // clear the column of any text and change the font to the same as input boxes
-    $('#markbookTable tr:not(:first) > td:nth-child(2)').each(function () {
+    $('#markbookTable tr:not(:first) > td:nth-child(6)').each(function () {
         $(this).text('');
         $(this).css({
             'font-family': 'Monaco, Courier, monospace'
@@ -44,4 +44,3 @@ const calculatePercentages = () => {
         }
     });
 };
-
