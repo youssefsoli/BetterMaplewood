8302c1f6fca0217eceac7e053764818b0557c752
Michael Xu <michael.xu1816@gmail.com>
2020-12-24 20:19:32 -0700

    Added proper selectors

--- src/selectors.js
+++ src/selectors.js
@@ -1,18 +1,32 @@
 // Get correct jQuery selectors depending on the table layout
 let selectors;
 const getSelectors = () => {
-    if (settings.betterTableLayout) {
+    if (settings.betterTableLayout && settings.liveModification) {
         selectors = {
             mark: 'td:nth-last-child(3) > input:first',
             weight: 'td:nth-last-child(2) > input',
             denominator: 'td:nth-last-child(3) > input:last',
             percentage: 'td:nth-child(2)'
         };
-    } else {
+    } else if (settings.liveModification) {
         selectors = {
             mark: 'td:nth-child(2) > input',
             weight: 'td:nth-child(4) > input',
             denominator: 'td:nth-child(5) > input',
+            percentage: 'td:nth-child(6)'
+        };
+    } else if (settings.betterTableLayout) {
+        selectors = {
+            mark: 'td:nth-last-child(3) > span:first',
+            weight: 'td:nth-last-child(2)',
+            denominator: 'td:nth-last-child(3) > span:last',
+            percentage: 'td:nth-child(2)'
+        };
+    } else {
+        selectors = {
+            mark: 'td:nth-child(2)',
+            weight: 'td:nth-child(4)',
+            denominator: 'td:nth-child(5)',
             percentage: 'td:nth-child(6)'
         };
     }
