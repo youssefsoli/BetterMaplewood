584bc5bd326b30112b6b0a9865fb6649a4089ba0
Michael Xu <61663933+themicklepickle@users.noreply.github.com>
2020-12-26 16:37:15 -0700

    Fixed the logic for background.js
    Co-authored-by: Youssef Soliman <youssefdevelops@gmail.com>

--- background.js
+++ background.js
@@ -1,72 +1,26 @@
-chrome.runtime.onInstalled.addListener(function () {
-    let quickview = document.getElementById('quickview');
-    let calculation = document.getElementById('calculation');
-    let liveModification = document.getElementById('liveModification');
-    let percentages = document.getElementById('percentages');
-    let betterTableLayout = document.getElementById('betterTableLayout');
-    let percentagePosition = document.getElementById('percentagePosition');
-    let version = document.getElementById('version');
-
-    const updateSettings = () => {
-        let quickviewToggle = quickview.checked;
-        let calculationToggle = calculation.checked;
-        let liveModificationToggle = liveModification.checked;
-        let percentagesToggle = percentages.checked;
-        let betterTableLayoutToggle = betterTableLayout.checked;
-        let percentagePositionValue = percentagePosition.value;
-
-        let settings = {
-            quickview: quickviewToggle,
-            calculation: calculationToggle,
-            liveModification: liveModificationToggle,
-            percentages: percentagesToggle,
-            betterTableLayout: betterTableLayoutToggle,
-            percentagePosition: percentagePositionValue
-        };
-
-        chrome.storage.sync.set(settings, () => {
-            chrome.tabs.query({
-                active: true,
-                currentWindow: true
-            }, (tab) => {
-                const url = new URL(tab[0].url);
-                if (url.hostname === 'hosting.maplewood.com')
-                    chrome.tabs.update(tab[0].id, { // Refresh the page
-                        url: tab[0].url
-                    });
-            });
-        });
-
-        setElementDisplays();
-
-        /* Set the initial values for the checkboxes */
-        chrome.storage.sync.get(null, (settings) => {
-            const defaults = {
-                quickview: true,
-                calculation: true,
-                liveModification: true,
-                percentages: true,
-                betterTableLayout: true,
-                percentagePosition: '3'
-            };
-
-            // assign saved value if it exist, otherwise value from default object above
-            quickview.checked = settings.quickview !== undefined ? settings.quickview : defaults.quickview;
-            calculation.checked = settings.calculation !== undefined ? settings.calculation : defaults.calculation;
-            liveModification.checked = settings.liveModification !== undefined ? settings.liveModification : defaults.liveModification;
-            percentages.checked = settings.percentages !== undefined ? settings.percentages : defaults.percentages;
-            betterTableLayout.checked = settings.betterTableLayout !== undefined ? settings.betterTableLayout : defaults.betterTableLayout;
-            percentagePosition.value = settings.percentagePosition !== undefined ? settings.percentagePosition : defaults.percentagePosition;
-
-            // eslint-disable-next-line no-prototype-builtins
-            if (!Object.keys(defaults).every(key => settings.hasOwnProperty(key))) // Check that settings has all the required keys
-                updateSettings();
-    
-            setElementDisplays();
-        });
+// This is called whenever the extension is first installed or updated
+chrome.runtime.onInstalled.addListener(() => {
+    const defaultSettings = {
+        quickview: true,
+        calculation: true,
+        liveModification: true,
+        percentages: true,
+        betterTableLayout: true,
+        percentagePosition: '3',
     };
+
+    chrome.storage.sync.get(null, settings => {
+        // Set setting to default if it's not found
+        Object.keys(defaultSettings).map(key => {
+            if (!settings.hasOwnProperty(key))
+                settings[key] = defaultSettings[key];
+        });
+
+        chrome.storage.sync.set(settings);
+    });
 });
 
-chrome.runtime.onUpdateAvailable.addListener(function () {
+// Reload the extension when there is a new update
+chrome.runtime.onUpdateAvailable.addListener(() => {
     chrome.runtime.reload();
 });
